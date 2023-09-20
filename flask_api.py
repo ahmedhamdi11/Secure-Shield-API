2cca9e956a455cd74a3047ed023122be725e2672
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-09-20 22:33:01 +0300

    malware detection added

--- flask_api.py
+++ flask_api.py
@@ -1,6 +1,7 @@
 from phishing_email_detection import PhisingEmailDetection
 from frauds_detection import FraudsDetection
 from attack_detection import AttackDetection
+from malware_detection import MalwareDetection
 
 from flask import Flask
 from flask_restful import Api
@@ -10,6 +11,7 @@ api =Api(app)
 
 api.add_resource(FraudsDetection,'/frauds_detection')
 api.add_resource(AttackDetection,'/attack_detection')
+api.add_resource(MalwareDetection,'/malware_detection')
 api.add_resource(PhisingEmailDetection,'/phising_email_detection')
 
 
