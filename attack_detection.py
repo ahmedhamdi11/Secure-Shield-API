33180a8a117ae7f04f38daea71d51cbdbc862dd7
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-09-20 17:38:40 +0300

    attack detection model added

---
+++ attack_detection.py
@@ -0,0 +1,50 @@
+from keras.models import load_model
+from flask_restful import Resource, reqparse
+from werkzeug.datastructures import FileStorage
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+# load the h5 model
+attackDetectionModel = load_model('ai_models/cnn-lstm_attack_detection_model.h5')
+
+# argments
+post_args = reqparse.RequestParser()
+post_args.add_argument('file',
+                        type =FileStorage,
+                          help='file.csv is required',
+                            required=True,
+                            location ='files'
+                            )
+
+
+# preprocess the file 
+def preprocessFile(file):
+    data =pd.read_csv(file)
+    
+    le = LabelEncoder()
+    data['protocol_type']=le.fit_transform(data['protocol_type'])
+    data['service']=le.fit_transform(data['service'])
+    data['flag']=le.fit_transform(data['flag'])
+
+    return data
+
+class AttackDetection(Resource):
+    def post(self):
+        args =post_args.parse_args()
+
+        # preprocess the email content 
+        preprocessedFile = preprocessFile(args['file'])
+
+        # Make predictions
+        predictions = attackDetectionModel.predict(preprocessedFile)
+        predictedClass =predictions.argmax(axis= 1)
+
+        classes =['Dos Attack','Probe Attack','Privilege Attack','Access Attack','Normal']
+
+        prediction_details = {
+            'prediction': classes[predictedClass.item()],
+            'prediction_accuracy': '{:.2f}%'.format(float(predictions[0][predictedClass.item()]) *100),
+        }
+
+        return {'data': prediction_details}
--- flask_api.py
+++ flask_api.py
@@ -1,5 +1,6 @@
 from phishing_email_detection import PhisingEmailDetection
 from frauds_detection import FraudsDetection
+from attack_detection import AttackDetection
 
 from flask import Flask
 from flask_restful import Api
@@ -7,9 +8,10 @@ from flask_restful import Api
 app =Flask(__name__)
 api =Api(app)
 
-
-api.add_resource(PhisingEmailDetection,'/phising_email_detection')
 api.add_resource(FraudsDetection,'/frauds_detection')
+api.add_resource(AttackDetection,'/attack_detection')
+api.add_resource(PhisingEmailDetection,'/phising_email_detection')
+
 
 if __name__ =='__main__':
     app.run(debug=True,host='0.0.0.0')
