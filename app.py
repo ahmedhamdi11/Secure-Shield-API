ee1ba9e30f91f29b7f43eb548a88f38acb1a0e67
Ahmed Hamdi <ahmedhamdi725@gmail.com>
2024-06-23 04:49:33 +0300

    rest code

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from phishing_email_detection import PhishingEmailDetection
 from frauds_detection import FraudsDetection
 from attack_detection import AttackDetection
 from malware_detection import MalwareDetection
-# from attack_detection_ml import AttackDetectionML
-# from frauds_detection_ml import FraudsDetectionMl
+from attack_detection_ml import AttackDetectionML
+from frauds_detection_ml import FraudsDetectionMl
 from awareness import Awareness
 
 from flask import Flask
@@ -17,9 +17,9 @@ api.add_resource(Awareness,'/awareness')
 api.add_resource(AttackDetection,'/attack_detection')
 api.add_resource(MalwareDetection,'/malware_detection')
 api.add_resource(PhishingEmailDetection,'/phishing_email_detection')
-# api.add_resource(AttackDetectionML,'/attack_detection_ml')
-# api.add_resource(FraudsDetectionMl,'/frauds_detection_ml')
+api.add_resource(AttackDetectionML,'/attack_detection_ml')
+api.add_resource(FraudsDetectionMl,'/frauds_detection_ml')
 
 
 if __name__ =='__main__':
-    app.run(debug=False,host='0.0.0.0',port=8080)
+    app.run(debug=False,host='0.0.0.0')
